af76c47029597d12cb1d2e52c010266dfc7ba507
Owen Kelvin <owenkelvin@Owens-MacBook-Pro.local>
2024-08-26 17:03:18 +0300

    Fix

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,7 +3,7 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder, SwaggerDocumentOptions } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
@@ -29,6 +29,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config, options);
   SwaggerModule.setup('api', app, document);
   const port = process.env.PORT || 3000;
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
--- libs/backend/db/src/lib/models/index.ts
+++ libs/backend/db/src/lib/models/index.ts
@@ -1,2 +1,3 @@
 export * from './route.model';
 export * from './stop.model';
+export * from './route-stop.model';
